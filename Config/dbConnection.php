9c4dc1f5fd52c95ba8bef9ced2290fa5c0a584fa
Sonia Arenas <puente46191@gmail.com>
2020-08-29 22:49:03 +0200

    Creada la clase para la conexión con la base de datos. Close #4

--- Config/dbConnection.php
+++ Config/dbConnection.php
@@ -1,3 +1,38 @@
 <?php 
+
+Class db_connection
+{
+	private $host;
+	private $user;
+	private $pass;
+	private $bd;
+
+	function __construct()
+	{
+
+		$this->host = "localhost";
+		$this->user ="root";
+		$this->pass = "";
+		$this->bd = "filmsdb";
+
+	}
+
+	public function connect(){
+		try{
+
+			$connection = mysqli_connect($this->host, $this->user, $this->pass, $this->bd);
+
+			return $connection;
+
+		}catch(PDOException $exception){
+
+			print "¡Error al conectar con la base de datos!: ". $exception->getMessage(). "<br/>";
+
+			die();
+		}
+	}
+
 	
+
+}	
 ?>
