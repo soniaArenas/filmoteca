a694c44a8a6a0f2703184e3a200fcab6c62cf356
DESKTOP-B92EJ6V\Sonia <puente46191@gmail.com>
2020-09-03 12:50:35 +0200

    index con llamada al controlador y base de la vista principal

---
+++ Views/indexFilms.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Filmoteca</title>
+
+  <link rel="stylesheet" href="Assets/Css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+  <h1 id="title">Películas</h1>
+  <div id="content">
+  <div id="searchDiv">
+    <input type="text" placeholder="Buscar..." id="searchFilm">
+      <button class="btn" id="btnAsc">Ordenar por año ascendente</button>
+    <button class="btn" id="btnDes">Ordenar por año descendente</button>
+    <button class="btn" id="addBtn">Añadir película</button>
+  </div>
+
+  <div id="addFilmDiv">
+    <input type="text" placeholder="Nombre" id="name">
+    <input type="text" placeholder="Año" id="year">
+    <button class="btn" id="addFilm">Añadir</button>
+  </div>
+ 
+  <div id="result">
+    
+   
+    
+  </div>
+</div>
+
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
 	
+
+include("controllers/filmController.php");
+
+
 ?>
