05098e1d5a590119bad193ec3bacdc46748663e0
DESKTOP-B92EJ6V\Sonia <puente46191@gmail.com>
2020-09-03 12:58:39 +0200

    Cambios en el archivo de configuración de la conexión

--- Config/dbConnection.php
+++ Config/dbConnection.php
@@ -1,38 +1,10 @@
 <?php 
 
-Class db_connection
-{
-	private $host;
-	private $user;
-	private $pass;
-	private $bd;
+$host="localhost";
+	$user="admin";
+	$pass="admin";
+	$db="filmsdb";
 
-	function __construct()
-	{
+$connect=mysqli_connect($host,$user,$pass,$db) or die ("Problemas en la conexión");
 
-		$this->host = "localhost";
-		$this->user ="root";
-		$this->pass = "";
-		$this->bd = "filmsdb";
-
-	}
-
-	public function connect(){
-		try{
-
-			$connection = mysqli_connect($this->host, $this->user, $this->pass, $this->bd);
-
-			return $connection;
-
-		}catch(PDOException $exception){
-
-			print "¡Error al conectar con la base de datos!: ". $exception->getMessage(). "<br/>";
-
-			die();
-		}
-	}
-
-	
-
-}	
-?>
+ ?>
